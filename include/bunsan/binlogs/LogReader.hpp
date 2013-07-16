5eecb0b2ce5a95865fce561da0a2bf241f2a4425
Aleksey Filippov <sarum9in@gmail.com>
2013-07-16 19:19:16 +0400

    LogReader implementation.

---
+++ include/bunsan/binlogs/LogReader.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "bunsan/binlogs/BaseReader.hpp"
+#include "bunsan/binlogs/MessageTypePool.hpp"
+#include "bunsan/binlogs/MessageType.hpp"
+
+#include <google/protobuf/io/coded_stream.h>
+#include <google/protobuf/message.h>
+
+#include <memory>
+
+namespace bunsan {
+namespace binlogs {
+
+class LogReader: public BaseReader, public MessageTypePool {
+public:
+    /// \warning Should always be called before object usage.
+    virtual bool readHeader(std::string *error=nullptr)=0;
+
+    /// Allocate and read message of appropriate type.
+    std::unique_ptr<google::protobuf::Message> read(std::string *error=nullptr);
+
+    bool read(const std::unique_ptr<google::protobuf::Message> &message, std::string *error=nullptr);
+    virtual bool read(google::protobuf::Message &message, std::string *error=nullptr)=0;
+
+    /// What message will be read next (if available)?
+    virtual const MessageType *nextMessageType(std::string *error=nullptr)=0;
+};
+
+}
+}
---
+++ src/lib/LogReader.cpp
@@ -0,0 +1,23 @@
+#include "bunsan/binlogs/LogReader.hpp"
+
+namespace bunsan {
+namespace binlogs {
+
+bool LogReader::read(const std::unique_ptr<google::protobuf::Message> &message, std::string *error)
+{
+    return read(*message, error);
+}
+
+std::unique_ptr<google::protobuf::Message> LogReader::read(std::string *error)
+{
+    if (const MessageType *const type = nextMessageType(error)) {
+        auto message = type->newMessage();
+        if (read(message, error)) {
+            return message;
+        }
+    }
+    return nullptr;
+}
+
+}
+}
