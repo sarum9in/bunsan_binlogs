4c1ffc2d74032c4b0727f74eb9854a3b4bc48765
Aleksey Filippov <sarum9in@gmail.com>
2013-07-17 19:32:28 +0400

    v1::LogWriter implementation.

---
+++ include/bunsan/binlogs/v1/LogWriter.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "bunsan/binlogs/v1/HeaderData.pb.h"
+#include "bunsan/binlogs/v1/MessageTypePool.hpp"
+
+#include "bunsan/binlogs/LogWriter.hpp"
+
+#include <google/protobuf/io/coded_stream.h>
+#include <google/protobuf/message.h>
+
+#include <memory>
+
+namespace bunsan {
+namespace binlogs {
+namespace v1 {
+
+class LogWriter: public binlogs::LogWriter {
+public:
+    explicit LogWriter(std::unique_ptr<google::protobuf::io::CodedOutputStream> &&output);
+
+    bool writeHeader(const Header &header, std::string *error=nullptr) override;
+
+    bool write(const std::string &typeName,
+               const google::protobuf::Message &message,
+               std::string *error=nullptr) override;
+
+    bool close(std::string *error=nullptr) override;
+
+    State state() const override;
+
+    const MessageType *type(const std::string &typeName) const override;
+    range types() const override;
+
+private:
+    bool write_(const std::string *const typeName,
+                const google::protobuf::Message &message,
+                std::string *error);
+
+    bool hadError(const std::string &msg, std::string *error);
+
+    std::unique_ptr<google::protobuf::io::CodedOutputStream> output_;
+    std::unique_ptr<HeaderData> headerData_;
+    v1::MessageTypePool pool_;
+    State state_ = State::kOk;
+};
+
+}
+}
+}
---
+++ src/lib/v1/LogWriter.cpp
@@ -0,0 +1,128 @@
+#include "bunsan/binlogs/v1/LogWriter.hpp"
+#include "bunsan/binlogs/detail/make_unique.hpp"
+
+#include <boost/assert.hpp>
+#include <boost/format.hpp>
+
+namespace bunsan {
+namespace binlogs {
+namespace v1 {
+
+LogWriter::LogWriter(std::unique_ptr<google::protobuf::io::CodedOutputStream> &&output):
+    output_(std::move(output))
+{
+    BOOST_ASSERT(output_);
+    BOOST_ASSERT(!output_->HadError());
+}
+
+bool LogWriter::writeHeader(const Header &header, std::string *error)
+{
+    if (!pool_.Init(header, error)) {
+        return false;
+    }
+    headerData_ = detail::make_unique<HeaderData>();
+    *headerData_->mutable_proto() = header.proto;
+    for (const std::string &type: header.types) {
+        headerData_->add_types()->set_name(type);
+    }
+    if (!write_(nullptr, *headerData_, error)) {
+        state_ = State::kBad;
+        output_.reset();
+        if (error) {
+            *error = "Unable to write header: " + *error;
+        }
+        return false;
+    }
+    return true;
+}
+
+bool LogWriter::write(const std::string &typeName,
+                      const google::protobuf::Message &message,
+                      std::string *error)
+{
+    if (output_) {
+        BOOST_ASSERT(headerData_);
+        // FIXME We should check that message has type defined in header.
+        // Or should we create template-based wrapper for this class?
+        return write_(&typeName, message, error);
+    } else {
+        return false;
+    }
+}
+
+bool LogWriter::write_(const std::string *const typeName,
+                       const google::protobuf::Message &message,
+                       std::string *error)
+{
+    if (output_) {
+        BOOST_ASSERT(!output_->HadError());
+        if (typeName) {
+            const google::protobuf::uint32 messageType = pool_.typeId(*typeName);
+            if (messageType == v1::MessageTypePool::npos) {
+                state_ = State::kFail;
+                if (error) {
+                    *error = str(boost::format("Type \"%1%\" is not registered."));
+                }
+                return false;
+            }
+            output_->WriteLittleEndian32(messageType);
+            if (hadError("Unable to write message type.", error)) {
+                return false;
+            }
+        }
+        const google::protobuf::uint32 messageSize = message.ByteSize();
+        output_->WriteLittleEndian32(messageSize);
+        if (hadError("Unable to write message size.", error)) {
+            return false;
+        }
+        message.SerializeWithCachedSizes(output_.get());
+        if (hadError("Unable to write message.", error)) {
+            return false;
+        }
+        BOOST_ASSERT(!output_->HadError());
+        state_ = State::kOk;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool LogWriter::hadError(const std::string &msg, std::string *error)
+{
+    if (output_->HadError()) {
+        state_ = State::kBad;
+        output_.reset();
+        if (error) {
+            *error = msg;
+        }
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool LogWriter::close(std::string */*error*/)
+{
+    state_ = State::kEof;
+    output_.reset();
+    return true;
+}
+
+LogWriter::State LogWriter::state() const
+{
+    return state_;
+}
+
+const MessageType *LogWriter::type(const std::string &typeName) const
+{
+    return pool_.type(typeName);
+}
+
+LogWriter::range LogWriter::types() const
+{
+    return pool_.types();
+}
+
+}
+}
+}
