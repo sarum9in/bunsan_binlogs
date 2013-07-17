8a6c80237feae64ce9bc59e8a22d86521377e052
Aleksey Filippov <sarum9in@gmail.com>
2013-07-17 17:34:28 +0400

    LogWriter::writeHeader() refactoring.
    fileDescriptorSet -> header.

--- include/bunsan/binlogs/LogWriter.hpp
+++ include/bunsan/binlogs/LogWriter.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
 #include "bunsan/binlogs/BaseWriter.hpp"
+#include "bunsan/binlogs/Header.hpp"
 #include "bunsan/binlogs/MessageTypePool.hpp"
 #include "bunsan/binlogs/MessageType.hpp"
 
-#include <google/protobuf/descriptor.pb.h>
 #include <google/protobuf/io/coded_stream.h>
 #include <google/protobuf/message.h>
 
@@ -14,9 +14,7 @@ namespace binlogs {
 class LogWriter: public BaseWriter, public MessageTypePool {
 public:
     /// \warning Should always be called before object usage.
-    virtual bool writeHeader(
-        const google::protobuf::FileDescriptorSet &fileDescriptorSet,
-        std::string *error=nullptr)=0;
+    virtual bool writeHeader(const Header &header, std::string *error=nullptr)=0;
 
     virtual bool write(const google::protobuf::Message &message, std::string *error=nullptr);
 
