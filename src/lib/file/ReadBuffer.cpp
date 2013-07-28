dfc59dab54245904a53850c979f6330cbab63e8e
Aleksey Filippov <sarum9in@gmail.com>
2013-07-28 15:57:44 +0400

    file::ReadBuffer::open() refactoring.

--- src/lib/file/ReadBuffer.cpp
+++ src/lib/file/ReadBuffer.cpp
@@ -1,10 +1,6 @@
 #include "bunsan/binlogs/file/ReadBuffer.hpp"
 
-#include "bunsan/binlogs/detail/make_unique.hpp"
-
-#include <boost/scope_exit.hpp>
-
-#include <fcntl.h>
+#include "openFd.hpp"
 
 namespace bunsan {
 namespace binlogs {
@@ -15,21 +11,8 @@ bool ReadBuffer::open(const boost::filesystem::path &path)
     if (!closed()) {
         return false;
     }
-    errno_ = 0;
-    int fd = ::open(path.c_str(), O_RDONLY);
-    BOOST_SCOPE_EXIT_ALL(&fd)
-    {
-        if (fd) {
-            ::close(fd);
-        }
-    };
-    if (fd < 0) {
-        errno_ = errno;
-        return false;
-    }
-    istream_ = detail::make_unique<google::protobuf::io::FileInputStream>(fd);
-    fd = 0; // disable scope exit
-    return true;
+    istream_ = openFd<google::protobuf::io::FileInputStream>(path, O_RDONLY, errno_);
+    return !getErrno();
 }
 
 google::protobuf::io::ZeroCopyInputStream *ReadBuffer::istream()
---
+++ src/lib/file/openFd.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "bunsan/binlogs/detail/make_unique.hpp"
+
+#include <boost/filesystem/path.hpp>
+#include <boost/scope_exit.hpp>
+
+#include <fcntl.h>
+
+namespace bunsan {
+namespace binlogs {
+namespace file {
+
+template <typename Stream>
+std::unique_ptr<Stream> openFd(const boost::filesystem::path &path, const int flags, int &errno_)
+{
+    std::unique_ptr<Stream> stream;
+    errno_ = 0;
+
+    int fd = 0;
+    BOOST_SCOPE_EXIT_ALL(&fd)
+    {
+        if (fd) {
+            ::close(fd);
+        }
+    };
+
+    fd = ::open(path.c_str(), flags);
+    if (fd < 0) {
+        errno_ = errno;
+        return false;
+    }
+    stream = detail::make_unique<Stream>(fd);
+    fd = 0; // disable scope exit
+
+    return std::move(stream);
+}
+
+}
+}
+}
