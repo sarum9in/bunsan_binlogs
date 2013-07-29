ab2e90c1a986f57f05e57c6e9f35bcb438da4c52
Aleksey Filippov <sarum9in@gmail.com>
2013-07-29 17:09:18 +0400

    io::file::WriteBuffer::openFd() bug fix.

--- src/lib/io/file/WriteBuffer.cpp
+++ src/lib/io/file/WriteBuffer.cpp
@@ -16,7 +16,7 @@ std::unique_ptr<google::protobuf::io::FileOutputStream> WriteBuffer::openFd(
     const boost::filesystem::path &path,
     int &errno_)
 {
-    return file::openFd<google::protobuf::io::FileOutputStream>(path, O_RDONLY, errno_);
+    return file::openFd<google::protobuf::io::FileOutputStream>(path, O_CREAT | O_WRONLY | O_TRUNC, errno_);
 }
 
 }
